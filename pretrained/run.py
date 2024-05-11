4657ce50a0fe43d0ffce2b41d494bbc87a80f232
Urban Lutz <urban.lutz@outlook.com>
2024-05-11 21:00:38 +0000

    cleanup

--- pretrained/run.py
+++ pretrained/run.py
@@ -1,18 +1,15 @@
 #!/usr/bin/env python
 
 import sys
-import os
 import yaml
-import pprint
-
-from torch.utils.data import DataLoader
+import torch
+import csv
 import lightning as L
 
 from lightning.pytorch.callbacks.early_stopping import EarlyStopping
 from lightning.pytorch.loggers import TensorBoardLogger
 
 sys.path.append('./pretrained/src/')
-from country211_module import Country211DataModule
 from eurosat_module import EuroSAT_RGB_DataModule, SentinelTest
 from vision_transformer import VisionTransformerPretrained
 
@@ -36,14 +33,7 @@ def main(arg):
         config = default_config
         print('Configuration read from ./configs/default.yaml')
 
-    print()
-    print('Configuration:')
-    pprint.pprint(config)
-    print()
-
-    if config['data_set'] == 'country211':
-        datamodule = Country211DataModule(config['data_root'], config['batch_size'])
-    elif config['data_set'] == 'tif':
+    if config['data_set'] == 'tif':
         # setup data
         path_to_data = config['data_root']
         datamodule = EuroSAT_RGB_DataModule(path_to_data, config['batch_size'], config['valid_size'])
@@ -87,29 +77,27 @@ def main(arg):
     trainer.save_checkpoint(f"final_checkpoint/version_{logger.version}-{execution_start}.chkpt")
 
 
+    # Generate Kaggle Submission
     test_dataset = SentinelTest(config["test_data_path"], 2)
     test_dataloader = test_dataset.test_dataloader()
+
+    # predict testset samples
     preds = trainer.predict(model=model, dataloaders=test_dataloader)
 
     
+    # resolve predictions
     labels2ids = datamodule.train_data.dataset.class_to_idx
     ids2labels = {v: k for k, v in labels2ids.items()}
-    
     
     def oh2text(one_hot):
         idx = torch.argmax(one_hot).item()
         return ids2labels[idx]
-    
-    
-    
-    
-    import torch
-    
+        
     class_array = []
     for pred, idx in preds:
         class_array = [*class_array, *[( idx[i], oh2text(pred.loss['logits'][i]))for i in range(pred.loss['logits'].shape[0])]]
     
-    import csv
+    # Save as CSV
     with open(f'results/version_{logger.version}-{execution_start}.csv', 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(['test_id', 'label'])
