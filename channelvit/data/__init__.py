9e87d7b70a64ca686bd948e9537d116fca0d652e
kojano28 <jano.koller@student.unisg.ch>
2024-05-12 13:37:05 +0200

    prepared for submission

--- channelvit/data/__init__.py
+++
@@ -1,4 +0,0 @@
-from channelvit.data.camelyon17 import Camelyon17
-from channelvit.data.imagenet import ImageNet
-from channelvit.data.jumpcp import JUMPCP
-from channelvit.data.so2sat import So2Sat
--- channelvit/meta_arch/__init__.py
+++
@@ -1,4 +0,0 @@
-from channelvit.meta_arch.correlation_computer import CorrelationComputer
-from channelvit.meta_arch.dino import DINO
-from channelvit.meta_arch.linear_prob import LinearProb
-from channelvit.meta_arch.supervised import Supervised
